be416fbaeb9bc17fdf843c5d75c254aaa6674837
Diego <40837217+dromerosm@users.noreply.github.com>
2023-10-20 13:31:47 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+import pytz
 
 class handler(BaseHTTPRequestHandler):
 
@@ -7,6 +9,14 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        response_dict = {"message": "Hello guys!"}
+
+        # Get the current time in Madrid
+        madrid_tz = pytz.timezone('Europe/Madrid')
+        madrid_time = datetime.now(madrid_tz).strftime('%Y-%m-%d %H:%M:%S')
+
+        response_dict = {
+            "message": "Hello guys!",
+            "timestamp": madrid_time
+        }
         self.wfile.write(json.dumps(response_dict).encode('utf-8'))
         return
