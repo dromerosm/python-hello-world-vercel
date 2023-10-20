0f0d4db94842a5ce04a2c24dece0cf13657ca556
Diego <40837217+dromerosm@users.noreply.github.com>
2023-10-20 13:25:38 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
+import json
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write('Hello guys!'.encode('utf-8'))
+        response_dict = {"message": "Hello guys!"}
+        self.wfile.write(json.dumps(response_dict).encode('utf-8'))
         return
